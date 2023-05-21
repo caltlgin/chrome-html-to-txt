2751ffe931a93039dde1a5a227943d23db0332f1
Chris Love <contact@clove.nz>
2023-05-21 21:27:12 +1200

    Convert code to "View webpage as TXT" Chrome extension

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
+// HTML to TXT: https://www.w3.org/services/html2txt
+// Example code: https://github.com/MitchellMarkGeorge/EmailThisPage
+// Text icons created by edt.im - Flaticon.com
+
 chrome.runtime.onInstalled.addListener(function ({ reason }) {
   if (reason === "install") {
     // in this case, we know that there are no context menu items hanging around
@@ -13,48 +17,43 @@ chrome.runtime.onInstalled.addListener(function ({ reason }) {
 });
 
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
-  console.log(tab?.title)
-  if (info.menuItemId === "emailThisPage" && info.pageUrl && tab?.title) {
-    openMailApp(info.pageUrl, tab?.title);
-  } else if (info.menuItemId === "emailThisLink" && info.linkUrl) {
-    openMailApp(info.linkUrl);
+  if (info.menuItemId === "html2txtThisPage" && info.pageUrl) {
+    html2txtLink(info.pageUrl, tab.index);
+  } else if (info.menuItemId === "html2txtThisLink" && info.linkUrl) {
+    html2txtLink(info.linkUrl, tab.index);
   }
 });
 
 chrome.action.onClicked.addListener(function (tab) {
-  if (tab && tab.title) {
+  if (tab) {
     // use pendingUrl if the normal url is not avalible (like the page is loading)
     if (tab.url) {
-      openMailApp(tab.url, tab.title);
+      html2txtLink(tab.url, tab.index);
     } else if (tab.pendingUrl) {
-      console.log(url);
-      openMailApp(tab.pendingUrl, tab.title);
+      html2txtLink(tab.pendingUrl, tab.index);
     }
   }
 });
 
-function openMailApp(url, title) {
-  let mailtoURL = new URL("mailto:");
-  if (title) {
-    
-    mailtoURL.searchParams.set("subject", title);
-    mailtoURL.searchParams.set("body", url);
-  } else {
-    mailtoURL.searchParams.set("body", url);
-  }
-  chrome.tabs.create({ url: mailtoURL.toString() });
+function html2txtLink(url, index) {
+  let html2txtURL = new URL("https://www.w3.org/services/html2txt");
+  html2txtURL.searchParams.set("url", url);
+  html2txtURL.searchParams.set("noinlinerefs", "on");
+  html2txtURL.searchParams.set("endrefs", "on");
+  // https://stackoverflow.com/questions/35780106/opening-tab-next-to-active-tab#73201419
+  chrome.tabs.create({ url: html2txtURL.toString(), index: index + 1 });
 }
 
 function createContextMenu() {
   chrome.contextMenus.create({
-    id: "emailThisPage",
-    title: "Email this Page",
+    id: "html2txtThisPage",
+    title: "View page as txt",
     type: "normal",
   });
 
   chrome.contextMenus.create({
-    id: "emailThisLink",
-    title: "Email this Link",
+    id: "html2txtThisLink",
+    title: "View link as txt",
     type: "normal",
     contexts: ["link"],
   });
